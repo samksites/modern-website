56cb234fb354887ff240ff7f2e2ded02ae4b3c9d
Samksites <Samuelksites@gmail.com>
2024-09-25 23:44:27 -0400

    file structure and nav

--- modern-website/src/App.js
+++ modern-website/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className=" size-ful">
+      <HomePage />
     </div>
   );
 }
---
+++ modern-website/src/components/navigation/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = (props) => {
+
+
+    return(
+        <div className="h-8 w-full flex justify-end text-white">
+            <h3>
+                Hi
+            </h3>
+            <h3>
+                Hi
+            </h3>
+            <h3>
+                Hi
+            </h3>
+
+        </div>
+    );
+}
+
+export default NavBar
