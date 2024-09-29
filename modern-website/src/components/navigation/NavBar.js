3b352b66fd7560c62c0fc1b43f34812134290169
Samksites <Samuelksites@gmail.com>
2024-09-28 23:25:10 -0400

    created intial nav bar

--- modern-website/src/components/navigation/NavBar.js
+++ modern-website/src/components/navigation/NavBar.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const NavBar = (props) => {
 
+    const navLinks = (text, css) => {
+
+        return(
+        <div className={"w-16 flex justify-center content-center hover:cursor-pointer [&>*]:hover:text-lg mr-1 ml-1 " + css}>
+            <h3>
+                {text}
+            </h3>
+        </div>)
+    }
+
 
     return(
-        <div className="h-8 w-full flex justify-end text-white">
-            <h3>
-                Hi
-            </h3>
-            <h3>
-                Hi
-            </h3>
-            <h3>
-                Hi
-            </h3>
+        <div className="h-8 w-full mt-1 flex justify-end text-white">
+            {navLinks("About")}
+            {navLinks("Work")}
+            {navLinks("Projects", "mr-8")}
 
         </div>
     );
